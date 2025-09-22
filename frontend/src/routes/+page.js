7b595a9a9fafe241b7504d04c658912b5de6f5fe
Andrea De Matteis <andrea.dematteis1194@gmail.com>
2025-09-22 11:48:58 +0200

    Add footer for credit and version + automatic update of package.json version

--- frontend/src/routes/+page.js
+++ frontend/src/routes/+page.js
@@ -1,11 +1,13 @@
 /** @type {import('./$types').PageLoad} */
 import {getRandomSentence} from "$lib/utils";
+import { version } from '../../package.json';
 
 
 export async function load() {
     const randomSentence = getRandomSentence();
-    
+
     return {
-        randomSentence: randomSentence
+        randomSentence: randomSentence,
+        version: version
     };
 };
